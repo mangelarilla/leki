3816bb668e5061c9dc2fa13325dffee6a80f34b7
maadsharp <mangelarilla@users.noreply.github.com>
2024-03-10 13:19:18 +0100

    backup purge check

--- src/messages/events/delete_event.rs
+++ src/messages/events/delete_event.rs
@@ -1,14 +1,13 @@
 use std::time::Duration;
-use serenity::all::{ButtonStyle, ChannelId, CommandInteraction, Context, CreateInteractionResponse, CreateInteractionResponseMessage, GetMessages, Mention, Message, MessageId, MessageType, ScheduledEventStatus};
+use serenity::all::{ButtonStyle, ChannelId, CommandInteraction, Context, CreateInteractionResponse, CreateInteractionResponseMessage, GetMessages, Mention, Message, MessageId, MessageType, PartialChannel, ScheduledEventStatus};
 use serenity::builder::CreateButton;
-use tracing::{info, instrument};
+use tracing::{instrument};
 use crate::prelude::*;
 
 #[instrument]
 pub async fn delete_event(interaction: &CommandInteraction, ctx: &Context, store: &Store) -> Result<()> {
-    let message = interaction.data.resolved.messages.keys().next().unwrap();
-    info!("Event to delete: {message}");
-    if let Ok(event) = store.get_event(*message).await {
+    let message = interaction.data.resolved.messages.values().next().unwrap();
+    if let Ok(event) = store.get_event(message.id).await {
         if let Some(id) = event.scheduled_event {
             let guild = interaction.guild_id.unwrap();
 
@@ -29,18 +28,20 @@ Estas bien {}?? Quieres hablar??
                 let response = interaction.get_response(&ctx.http).await?;
                 if let Some(interaction) = response.await_component_interaction(&ctx.shard).timeout(Duration::from_secs(60)).await {
                     guild.delete_scheduled_event(&ctx.http, id).await?;
-                    remove_event(store, ctx, interaction.channel_id, *message).await?;
+                    remove_event(store, ctx, interaction.channel_id, message.id).await?;
 
                     interaction.create_response(&ctx.http, CreateInteractionResponse::UpdateMessage(purged().embeds(vec![]))).await?;
                 }
             } else {
-                remove_event(store, ctx, interaction.channel_id, *message).await?;
+                remove_event(store, ctx, interaction.channel_id, message.id).await?;
                 interaction.create_response(&ctx.http, CreateInteractionResponse::Message(purged())).await?;
             }
         } else {
-            remove_event(store, ctx, interaction.channel_id, *message).await?;
+            remove_event(store, ctx, interaction.channel_id, message.id).await?;
             interaction.create_response(&ctx.http, CreateInteractionResponse::Message(purged())).await?;
         }
+    } else if message.author.id.get() == 1148032756899643412 && is_event_channel(interaction.channel.clone().unwrap()) {
+        purge_channel(ctx, interaction.channel_id).await?;
     } else {
         interaction.create_response(&ctx.http, super::not_an_event_response()).await?;
     }
@@ -48,18 +49,26 @@ Estas bien {}?? Quieres hablar??
     Ok(())
 }
 
-async fn get_event_channel_messages(channel: ChannelId, ctx: &Context) -> Result<Vec<Message>> {
-    let messages = channel.messages(&ctx.http, GetMessages::new()).await?
-        .into_iter()
-        .filter(|msg| !msg.pinned && msg.kind != MessageType::PinsAdd)
-        .collect::<Vec<Message>>();
-    Ok(messages)
+fn is_event_channel(channel: PartialChannel) -> bool {
+    let name = channel.name.unwrap();
+
+    name.ends_with("-a") || name.ends_with("-b") || name.ends_with("-c")
 }
 
 async fn remove_event(store: &Store, ctx: &Context, channel: ChannelId, message: MessageId) -> Result<()> {
-    let channel_messages = get_event_channel_messages(channel, ctx).await?;
-    channel.delete_messages(&ctx.http, channel_messages).await?;
+    purge_channel(ctx, channel).await?;
     store.remove_event(message).await?;
+
+    Ok(())
+}
+
+async fn purge_channel(ctx: &Context, channel: ChannelId) -> Result<()> {
+    let channel_messages = channel.messages(&ctx.http, GetMessages::new()).await?
+        .into_iter()
+        .filter(|msg| !msg.pinned && msg.kind != MessageType::PinsAdd)
+        .collect::<Vec<Message>>();
+
+    channel.delete_messages(&ctx.http, channel_messages).await?;
 
     Ok(())
 }
