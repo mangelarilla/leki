58d1e021fb3765655883664ba0896573b468494a
maadsharp <mangelarilla@users.noreply.github.com>
2024-03-11 12:57:13 +0100

    limit placeholder length

--- src/utils/components.rs
+++ src/utils/components.rs
@@ -2,12 +2,21 @@ use serenity::all::{CreateActionRow, CreateInputText, InputTextStyle};
 
 pub(crate) fn short_input(label: &str, id: &str, placeholder: &str, required: bool) -> CreateActionRow {
     CreateActionRow::InputText(CreateInputText::new(InputTextStyle::Short, label, id)
-        .placeholder(placeholder)
+        .placeholder(truncate(placeholder, 50))
         .required(required))
 }
 
 pub(crate) fn long_input(label: &str, id: &str, placeholder: &str, required: bool) -> CreateActionRow {
     CreateActionRow::InputText(CreateInputText::new(InputTextStyle::Paragraph, label, id)
-        .placeholder(placeholder)
+        .placeholder(truncate(placeholder, 80))
         .required(required))
+}
+
+fn truncate(s: &str, max_chars: usize) -> String {
+    let s = match s.char_indices().nth(max_chars - 3) {
+        None => s,
+        Some((idx, _)) => &s[..idx],
+    };
+
+    format!("{s}...")
 }
